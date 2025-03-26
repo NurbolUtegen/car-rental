4c19c4998bdfad15c2fc78077fb5e658f7a4b552
Aidarik422 <aidaranuar07@gmail.com>
2025-03-26 22:12:55 +0500

    Update admin.py

--- myapp/admin.py
+++ myapp/admin.py
@@ -1,12 +1,16 @@
 from django.contrib import admin
 from .models import Car, Rental
+class RentalInline(admin.TabularInline):  
+    model = Rental  
+    extra = 1  
 
-@admin.register(Car)
-class CarAdmin(admin.ModelAdmin):
-    list_display = ("brand", "model", "year", "price_per_day", "available")
-    list_filter = ("available", "brand", "year")
-    search_fields = ("brand", "model")
-
+@admin.register(Car)  
+class CarAdmin(admin.ModelAdmin):  
+    list_display = ("brand", "model", "year", "price_per_day", "available")  
+    list_filter = ("available", "brand", "year")  
+    search_fields = ("brand", "model")  
+    inlines = [RentalInline]
+    
 @admin.register(Rental)
 class RentalAdmin(admin.ModelAdmin):
     list_display = ("user", "car", "start_date", "end_date", "total_price")
